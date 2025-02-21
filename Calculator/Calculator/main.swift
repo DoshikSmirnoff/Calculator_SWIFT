c08ead585737270658c246645bf1757821bc9038
Daria <DoshikSmirnoff@gmail.com>
2025-02-21 20:09:40 +0300

    COMMIT #3: Update function; new while-loop and two new conditional construction

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -7,37 +7,64 @@
 
 import Foundation
 
-func data_entry(a: Int, b: Int, sign: String) -> Int {
-    var a = readLine()
-    var b = readLine()
-    var sign = readLine()
+
+
+func dataEntry(a: Int, b: Int, sign: String) -> Int? {
     
-    if let int_a = a and && let int_b = b {
-        print(int_a)
-        print(int_b)
-    }
-    if var int_a = Int(a) && int_b = Int(b) {
-        
-    }
-    
-    if sign == "+" {
+   if sign == "+" {
         return a + b
     } else if sign == "-" {
         return a - b
     } else if sign == "*" {
         return a * b
     } else if sign == "/" {
-        return a / b
+        if b == 0 {
+            return nil
+        } else {
+            return a / b
+        }
+    } else {
+        return nil
     }
     
 }
+//
+//import Foundation
+//
+//let line = readLine()
+//if let newLine = line {
+// print(newLine)
+// if let int = Int(newLine) {
+//  print(int)
+// }
+//}
 
-import Foundation
-
-let line = readLine()
-if let newLine = line {
- print(newLine)
- if let int = Int(newLine) {
-  print(int)
- }
+while true {
+    var a = Int(readLine() ?? "0")
+    var b = Int(readLine() ?? "0")
+    var sign = readLine()
+    
+    if let aLast = a, let bLast = b, let signLast = sign {
+        let result = dataEntry(a: aLast, b: bLast, sign: signLast)
+        if let resultLast = result {
+            print("Ваш результат равен: \(resultLast)!")
+        } else {
+            print("Не выполнимый запрос!")
+        }
+    }
+    
+    
+    print("Если хотите продолжить, введите Yes, если хотите завершить - No")
+    
+    var response = readLine()
+    
+    if let responseLast = response {
+        if responseLast == "Yes" {
+            continue
+        } else if responseLast == "No" {
+            break
+        } else {
+            print("Неверный ответ!")
+        }
+    }
 }
